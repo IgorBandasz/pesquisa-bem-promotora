2da441cdb481a1bf092ff1879b234a4e0081aeb8
Igor Bandasz <44757501+IgorBandasz@users.noreply.github.com>
2020-07-06 19:23:21 -0300

    testar botão radio

---
+++ site_pesquisa/index.php
@@ -0,0 +1,35 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Pesquisa Bem Promotora</title>
+    </head>
+    <body>
+        <h1>Olá</h1>
+        <h2>A Bem Promotora está propondo essa rápida pesquisa a fim de conhecer melhor seus clientes</h2>
+
+        1. Qual sua faixa de idade:
+        <div class="radio">
+	        <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked="">
+	        <label for="optionsRadios1">Até 30 anos</label>
+        </div>
+        <div class="radio">
+	        <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2" checked="">
+	        <label for="optionsRadios2">De 30 a 50 anos</label>
+        </div>
+        <div class="radio">
+        	<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3" >
+        	<label for="optionsRadios3">De 50 a 65 anos</label>
+        </div>
+        <div class="radio">
+        	<input type="radio" name="optionsRadios" id="optionsRadios4" value="option4" >
+        	<label for="optionsRadios4">Acima de 65 anos</label>
+        </div>
+    </body>
+</html>
